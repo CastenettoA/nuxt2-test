5117f44064dc3c2b5cb77227eb5083f0cfd1ea0a
CastenettoA <castenetto.andrea@gmail.com>
2023-03-02 12:48:21 +0100

    init

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+
+        <NuxtLink to="/">
+            <div class="log flex items-center py-2 px-4">
+                <img class="w-10" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-64.png"
+                    alt="logo">
+                <span class="font-bold ml-2">Nuxt2 Test</span>
+            </div>
+        </NuxtLink>
+
+        <Menu />
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
